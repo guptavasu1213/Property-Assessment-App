63ed93122029ee3290f5caff5cc668f7ae295411
Vasu Gupta <guptavasu1213@gmail.com>
2020-02-01 19:06:33 -0700

    Add drivers of program

---
+++ src/project/Lab3Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author vasug
+ */
+public class Lab3Main {
+    public static void main(String[] args) {
+        Project lab3 = new Project();
+        String fileName = lab3.getFileName();
+        
+        lab3.setUpRecords(fileName);    
+        
+        // 
+        lab3.assessmentClassStats();
+    }    
+}
