a89914c28c821c0691ea8c180100917334e7c749
Vasu Gupta <guptavasu1213@gmail.com>
2020-02-01 19:05:53 -0700

    Change class structure

--- src/project/Record.java
+++ src/project/Record.java
@@ -13,125 +13,59 @@ import java.math.BigDecimal;
  */
 public class Record {
     private final Long _accNum;
-    private String _suite;
-    private Integer _houseNum;
-    private String _streetName;
-    private Long _assessedVal;
-    private String _assessmentClass;
-    private Integer _neighbourhoodID;
-    private String _neighbourhoodName;
-    private String _wardName;
-    private boolean _garagePresent; // Y = true; N = False
-    private BigDecimal _latitude;
-    private BigDecimal _longitude;
+    
+    private Address _address;
+    private Neighbourhood _neighbourhoodInfo;        
+    private Coordinates _coordinates;
+    
+    private Long _assessedVal;    
+    private String _garagePresent; 
     
     // Getters
     public Long getAccNum(){
         return _accNum;
     }
-    public String getSuite(){
-        return _suite;
-    }
-    public Integer getHouseNum(){
-        return _houseNum;
-    }
-    public String getStreetName(){
-        return _streetName;
-    }
     public Long getAssessedVal(){
         return _assessedVal;
     }
-    public String getAssessmentClass(){
-        return _assessmentClass;
-    }
-    public Integer getNeighbourhoodID(){
-        return _neighbourhoodID;  
-    }
-    public String getNeighbourhoodName(){
-        return _neighbourhoodName;
-    }
-    public String getWardName(){
-        return _wardName;
-    }
     public String getGarageStatus(){
-        if (_garagePresent == true){
-            return "Y";
-        }
-        else{
-            return "N";
-        }
+        return _garagePresent;
     }
-    public BigDecimal getLatitude(){
-        return _latitude;
+    public Address getAddress(){
+        return _address;
     }
-    public BigDecimal getLongitude(){
-        return _longitude;
+    public Neighbourhood getNeighbourhood(){
+        return _neighbourhoodInfo;
+    }
+    public Coordinates getCoordinates(){
+        return _coordinates;
     }
     
-    
-    
-    private static BigDecimal checkEmptyArgumentBigDecimal(String arg){
-        if (arg.trim().equals("")){
-            return null;
-        }
-        else{
-            return new BigDecimal(arg);
-        }
+    // Constructor ================????????
+    public Record() {
+        this._accNum = null;
     }
-    
-    private static Integer checkEmptyArgumentInteger(String arg){
-        if (arg.trim().equals("")){
-            return null;
-        }
-        else{
-            return Integer.parseInt(arg);
-        }
-    }
-    private static Long checkEmptyArgumentLong(String arg){
-        if (arg.trim().equals("")){ // CONFIRM FOR STRINGS£££££££££££££
-            return null;
-        }
-        else{
-            return Long.parseLong(arg);
-        }
-    }
-    /**
-     *
-     * @param arg
-     * @return
-     */
-    private static String checkEmptyArgumentString(String arg){
-        if (arg.trim().equals("")){
-            return null;
-        }
-        else{
-            return arg;
-        }
-    }
-    
+        
     // Constructor
-    public Record(String accNum, String suite, String houseNum, String streetName, 
-            String assessedVal, String assessmentClass, String neighbourhoodID, 
+    public Record(Long accNum, String suite, Integer houseNum, String streetName, 
+            Long assessedVal, String assessmentClass, Integer neighbourhoodID, 
             String neighbourhoodName, String wardName, String garagePresent, 
-            String latitude, String longitude){
-        this._suite =  checkEmptyArgumentString(suite);
-        this._accNum = checkEmptyArgumentLong(accNum);
-        this._houseNum = checkEmptyArgumentInteger(houseNum);
-        this._streetName = checkEmptyArgumentString(streetName);
-        this._assessedVal = checkEmptyArgumentLong(assessedVal);
-        this._assessmentClass = checkEmptyArgumentString(assessmentClass);
-        this._neighbourhoodID = checkEmptyArgumentInteger(neighbourhoodID);
-        this._neighbourhoodName = checkEmptyArgumentString(neighbourhoodName);
-        this._wardName = checkEmptyArgumentString(wardName);
-        this._garagePresent = garagePresent.equals("Y"); // return true or false
-        this._latitude = checkEmptyArgumentBigDecimal(latitude);
-        this._longitude = checkEmptyArgumentBigDecimal(longitude);
+            BigDecimal latitude, BigDecimal longitude){
+
+        this._accNum = accNum;
+        this._address = new Address(suite, houseNum, streetName);
+        this._neighbourhoodInfo = new Neighbourhood(assessmentClass,
+            neighbourhoodID, neighbourhoodName, wardName);
+        this._coordinates = new Coordinates(latitude, longitude);
+        
+        this._assessedVal = assessedVal;
+        this._garagePresent = garagePresent;
     }
     @Override
     public String toString(){
         return String.format(
                 "Account Number: %10d\tSuite: %7s\tLongitude: %f", 
-                this._accNum, this._suite, this._longitude);
+                this._accNum, this._address.getSuite(), this._coordinates.getLongitude());
 //        return "Account Number: " + this.accNum +
 //                "\tSuite: " + this.suite +
 //                "\tAssessed Value: " + this.assessedVal +
